9c6290e80b3fdf13f09f76aeffa5a143a3f99f98
Matheus Alves <matheusmota.webdev@gmail.com>
2023-07-03 05:31:32 -0300

    first commit

---
+++ gizmodo_uol.py
@@ -0,0 +1,56 @@
+import requests
+from bs4 import BeautifulSoup
+from selenium import webdriver
+import pandas as pd
+from selenium.webdriver.chrome.options import Options
+from time import sleep
+
+options = Options()
+options.add_argument('window-size=400,800')
+
+navegador = webdriver.Chrome(options=options)
+navegador.get('https://gizmodo.uol.com.br')
+
+sleep(2)
+
+lista_noticias = []
+
+
+content = navegador.page_source
+site = BeautifulSoup(content, 'html.parser')
+
+noticias = site.findAll('div', attrs={'class': 'list-item'})
+
+for i in range(10):
+    noticia = noticias[i]
+
+    titulo = noticia.find('a', attrs={'rel': 'bookmark'})
+    data = noticia.find('span', attrs={'class': 'metaText metaDate'})
+    resumo = noticia.find('div', attrs={'class': 'postSummary entry-content'})
+
+    print('Titulo da noticia:')
+    print(titulo.text)
+    print('----')
+    print('data da notícia:')
+    print(data.text)
+    print('----')
+    print('Resumo da notícia:')
+    print(resumo.text)
+    print('----')
+
+    lista_noticias.append([titulo.text, data.text, resumo.text])
+
+navegador.quit()
+
+
+news = pd.DataFrame(lista_noticias, columns=['Título da notícia', 'data da notícia','Resumo da notícia'])
+
+news.to_csv('noticias.csv', index=False)
+
+
+
+
+
+
+
+
